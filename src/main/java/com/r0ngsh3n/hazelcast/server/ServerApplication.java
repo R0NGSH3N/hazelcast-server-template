fed80011d64804e942ee4a8dccafa9fa8d43e64e
rong shen <rong.shen@outlook.com>
2020-11-07 20:03:43 -0500

    init commit

---
+++ src/main/java/com/r0ngsh3n/hazelcast/server/ServerApplication.java
@@ -0,0 +1,13 @@
+package com.r0ngsh3n.hazelcast.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/r0ngsh3n/hazelcast/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.r0ngsh3n.hazelcast.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
