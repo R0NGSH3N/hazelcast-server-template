da8da1dad5207d58d67d52d6cae8850ce8873153
r0ngsh3n <rong.shen@outlook.com>
2020-11-10 09:41:59 -0500

    remove the upper folder

--- src/main/java/com/r0ngsh3n/hazelcast/server/notifier/channel/HazelCastEventNotifierChannel.java
+++
@@ -1,9 +0,0 @@
-package com.r0ngsh3n.hazelcast.server.notifier.channel;
-
-
-import com.r0ngsh3n.hazelcast.server.listener.HazelCastEvent;
-
-public interface HazelCastEventNotifierChannel {
-
-    public void sendEvent(HazelCastEvent event);
-}
