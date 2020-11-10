33b9f48233c89eee719c1ccdc3e384ee894ae63c
rongshen <rong.shen@outlook.com>
2020-11-09 23:52:02 -0500

    update the event listener to adapt all hazelcast events

--- src/main/java/com/r0ngsh3n/hazelcast/server/notifier/HazelCastEventNotifier.java
+++ src/main/java/com/r0ngsh3n/hazelcast/server/notifier/HazelCastEventNotifier.java
@@ -8,24 +8,21 @@ import java.util.List;
 import java.util.Map;
 
 public class HazelCastEventNotifier {
-    public enum EVENT_TYPE {
-        MEMBERSHIP_EVENT, DISTRIBUTED_OBJECT_EVENT, PATITION_LOST_EVENT
-    }
 
-    private Map<EVENT_TYPE, List<HazelCastEventNotifierChannel>> notifier;
+    private Map<HazelCastEvent.EVENT_TYPE, List<HazelCastEventNotifierChannel>> notifier;
 
-    public void setNotifier(Map<EVENT_TYPE, List<HazelCastEventNotifierChannel>> notifier) {
+    public void setNotifier(Map<HazelCastEvent.EVENT_TYPE, List<HazelCastEventNotifierChannel>> notifier) {
         this.notifier = notifier;
     }
 
-    public Map<EVENT_TYPE, List<HazelCastEventNotifierChannel>> getNotifier() {
+    public Map<HazelCastEvent.EVENT_TYPE, List<HazelCastEventNotifierChannel>> getNotifier() {
         if(notifier == null){
-            notifier = new HashMap<EVENT_TYPE, List<HazelCastEventNotifierChannel>>();
+            notifier = new HashMap<HazelCastEvent.EVENT_TYPE, List<HazelCastEventNotifierChannel>>();
         }
         return notifier;
     }
 
-    public void inform(EVENT_TYPE event_type, HazelCastEvent event){
-        this.notifier.get(event).stream().forEach(e -> e.sendEvent(event));
+    public void inform(HazelCastEvent event){
+        this.notifier.get(event.getEventType()).stream().forEach(e -> e.sendEvent(event));
     }
 }
